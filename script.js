48266e93538da15adb2711c0c14492a639e4b55c
Dane Kargis <dane.kargis@gmail.com>
2023-03-24 08:08:30 -0600

    Added the rest of the time blocks

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ function time() {
 }
 
 setInterval(time, 1000);
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
