1591fac01da985a661be15e422dc8f9ba245e1d0
Dane Kargis <dane.kargis@gmail.com>
2023-03-27 20:46:32 -0600

    got the text to store in local storage, still figuring out how to get it to display in the text after refreshing the page

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+var currentDay = $('#currentDay');
+var saveBtn = $('.saveBtn');
+var timeBlockEl = $('time-block');
+var textAreaEl = $('description');
+var hourEL = $('hour');
+var hour9El = $('#hour-9');
+var hour10EL = $( '#hour-10');
+var hour11El = $( '#hour-11');
+var hour12EL = $ ('#hour-12');
+var hour13EL = $( '#hour-13');
+var hour14El = $ ('#hour-14');
+var hour15El = $( '#hour-15');
+var hour16EL = $( '#hour-16');
+var hour17EL = $('#hour-17');
+var hour18EL = $( '#hour-18');
+var hour19EL = $( '#hour-19');
+var hour20EL = $( '#hour-20');
+var pastEl = $( 'past');
+var presentEl = $( 'present');
+var futureEl = $( 'future');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 var today = dayjs();
 $('#1a').text(today.format('MMM D, YYYY'));
 
-// var time = dayjs().format('hh:mm:ss');
-// $('#2a').text(time);
 
 var span = document.getElementById('2a');
 
@@ -20,9 +39,48 @@ function time() {
 
 setInterval(time, 1000);
 
+// I had a fellow student help me with this funtion, I was not able to figure it out on my own.
+$(function timeOfDay () {
+  var currentTime = dayjs().hour();
+  console.log(currentTime);
+  for (var i = 9; i <= 21; i++) {
+    if (currentTime > i) {
+      $('#hour-' + i).addClass('past');
+    } else if (currentTime < i) {
+      $('#hour-' + i).addClass('future');
+    } else {
+      $('#hour-' + i).addClass('present');
+    } 
+  }
+  console.log(timeOfDay)
+});
+
+for (var i = 0; i < saveBtn.length; i++) {
+  saveBtn[i].addEventListener ('click', enteredText);
+  console.log(enteredText)
+  };
+
+  function enteredText () {
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, text);
+    };
+
+    function displayInfo () {
+      for (var i = 9; i <= 17; i++) {
+      $('#hour-' + i + '.description').val(localStorage.getItem('hour-'+ i));
+      }
+     
+
+    };
+
+    displayInfo();
+
+console.log(displayInfo);
 
 
-$(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -41,4 +99,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
